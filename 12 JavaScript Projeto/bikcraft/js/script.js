d48b09919dfeb7b769cab76a26042aee9b55944b
Diego Rafael Vieira <diegorafaelvieira@bol.com.br>
2024-01-10 23:01:48 -0300

    Add Parâmetros Url

--- 12 JavaScript Projeto/bikcraft/js/script.js
+++ 12 JavaScript Projeto/bikcraft/js/script.js
@@ -1,3 +1,4 @@
+//Ativar Links do Menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -7,7 +8,19 @@ function ativarLink(link) {
   if (url.includes(href)) {
     link.classList.add("ativo");
   }
-
 }
 
 links.forEach(ativarLink);
+
+
+//Ativar Itens do Orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
